9066021459342ccd1e15f73e5a60412148f53098
siri-syl <sylee010101@gmail.com>
2021-12-03 09:16:31 +0900

    UPDATE: edit controller auth.js, create token.js auth: 로그인, 로그아웃, 회원가입, 이메일 중복검사 기능을 만들었습니다. token: 토큰 생성, 보내기 기능을 만들었습니다.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,19 +1,63 @@
-
+const db = require('../models');
+const { generateAccessToken, sendAccessToken } = require('../controllers/token.js');
 module.exports = {
-    login: (req, res) => {
-        res.status(200).send('login');
+    login: async (req, res) => {
+        const { email, password } = req.body;
+        let userInfo = await db.user.findOne({
+            where: {email: email, password: password}
+        })
+
+        if(!userInfo) {
+            res.status(401).json({"message": "Invalid email or password"})
+            return ;
+        }
+        else {
+            const { id, nickname, area, area2, manager, socialType } = userInfo;
+            const userData = {
+                userId: id,
+                nickname: nickname,
+                area: area,
+                area2: area2,
+                manager: manager,
+                socialType: socialType
+            }
+            const token = generateAccessToken(userData);
+            sendAccessToken(res, token, userData);
+        }
     },
-    logout: (req, res) => {
-        res.status(200).send('logout');
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('jwt');
+            res.status(200).json({"message": "logout!"});
+            return;
+        } catch (err) {
+            console.log(err);
+            return res.status(401).json({ "message": "Unauthorized"});
+        }
     },
-    signup: (req, res) => {
-        res.status(200).send('shinup');
+    signup: async (req, res) => {
+        const { email, password } = req.body;
+        await db.user.create({
+            email: email, password:password
+        });
+        res.status(201).json({ "message": "signUp!"});
+        return;
     },
     email: (req, res) => {
         res.status(200).send('email인증');
     },
-    emailCheck: (req, res) => {
-        res.status(200).send('email체크');
+    emailCheck: async (req, res) => {
+        const { email } = req.body;
+        const emailCheck = await db.user.findOne({
+            where: { email: email }
+        });
+        if (emailCheck) {
+            res.status(409).json({ "message": "Email already exists"});
+            return;
+        }
+        else {
+            res.status(200).json({ "message": "ok!"})
+        }
     },
     passwordCheck: (req, res) => {
         res.status(200).send('비밀번호 체크');
---
+++ server/controllers/token.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const { sign } = require('jsonwebtoken');
+const DOMAIN = process.env.DOMAIN || 'localhost'
+
+module.exports = {
+    generateAccessToken: (data) => {
+        return sign(data, process.env.ACCESS_SECRET, { expiresIn: "1d" });
+      },
+    sendAccessToken: (res, token, userData) => {
+        userData = userData || {data: null};
+        
+        res.status(200).cookie("jwt", token,{
+          domain: DOMAIN,
+          path: '/',
+          secure: false,
+          httpOnly: true,
+          sameSite: 'none'
+    }).json(userData);
+    return ;
+    },
+    isAuthorized: (req) => {
+        const authorization = req.headers["authorization"];
+        if (!authorization) {
+          return null;
+        }
+        const token = authorization.split(" ")[1];
+        try {
+          return verify(token, process.env.ACCESS_SECRET);
+        }
+        catch (err) {
+          // return null if invalid token
+          return null;
+        }
+    }
+};

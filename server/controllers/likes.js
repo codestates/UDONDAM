11afeb18b76536dcdcdc8bd279b60c15523c491e
siri-syl <sylee010101@gmail.com>
2021-12-06 17:04:07 +0900

    UPDATE: controllers likes.js - 내가 따봉한 게시물 조회를 구현하려 했으나 게시물 조회만 되고 likesCount가 하나만 뜨는 오류가 있습니다. - like - Post (따봉 누르기) 기능을 구현했습니다. 아직 취소는 구현하지 않았습니다.

--- server/controllers/likes.js
+++ server/controllers/likes.js
@@ -11,10 +11,60 @@ module.exports = {
         if(!userInfo){
             res.status(401).json({"message" : "token doesn't exist"});
         }
-        res.status(200).send("likesUser");
+        else {
+            const result = await post.findAll({
+                include: [{
+                    model: likes,
+                    attributes: ['userId'],
+                    required: true,
+                    where: {
+                        userId: userInfo.id
+                    }
+                },
+                {
+                    model: comment,
+                    attributes: ['id'],
+                    required: true
+                }],
+                order: [['createAt','DESC']],
+                limit: 10
+            })
+            if(result.length === 0) {
+                return res.status(200).send(posts);
+            }
+            let result2 = [];
+
+            result.map((post)=> {
+                const { id, content, createAt, likes, comments } = post;
+                console.log(likes)
+                result2.push({
+                    id: id,
+                    content: content,
+                    createAt: createAt,
+                    likeCount: likes.length,
+                    commentCount: comments.length
+                });
+            });
+            return res.status(200).send(result2);
+        }
     },
-    likesCreate: (req, res) => {
-        res.status(200).send("likesCreate");
+    likesCreate: async (req, res) => {
+        const { email, postId } = req.query;
+        let userInfo = await user.findOne({
+            where: {
+                email: email
+            }
+        });
+        if(!userInfo){
+            res.status(401).json({"message" : "token doesn't exist"});
+        }
+        else {
+            await likes.create({
+                userId: userInfo.id,
+                postId: postId
+            })
+            res.status(200).send("likesCreate");
+        }
     },
     likesDelete: (req, res) => {
         res.status(200).send("likesDelete");

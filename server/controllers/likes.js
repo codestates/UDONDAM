86793e81095cc8eb01e12745a06acfe633642dc4
siri-syl <sylee010101@gmail.com>
2021-12-07 09:08:55 +0900

    UPDATE: controllers likes.js 따봉 추가, 따봉 취소를 구현했습니다.

--- server/controllers/likes.js
+++ server/controllers/likes.js
@@ -1,21 +1,22 @@
 const { comment, likes, post_tag, post, tag, user } = require("../models");
+const { sequelize } = require("sequelize");
 const { isAuthorized } = require('../controllers/token.js');
 module.exports = {
     likesUser: async (req, res) => {
-        const { email } = req.body;
+        const { email } = req.query;
         let userInfo = await user.findOne({
             where: {
                 email: email
             }
         })
         if(!userInfo){
-            res.status(401).json({"message" : "token doesn't exist"});
+            res.status(401).json({ "message" : "token doesn't exist" });
         }
         else {
             const result = await post.findAll({
                 include: [{
                     model: likes,
-                    attributes: ['userId'],
+                    group: 'postId',
                     required: true,
                     where: {
                         userId: userInfo.id
@@ -29,23 +30,24 @@ module.exports = {
                 order: [['createAt','DESC']],
                 limit: 10
             })
-            if(result.length === 0) {
-                return res.status(200).send(result);
-            }
-            let result2 = [];
+            // console.log(result)
+            // if(result.length === 0) {
+                return res.status(200).send("구현 중입니다");
+            // }
+            // let result2 = [];
 
-            result.map((post)=> {
-                const { id, content, createAt, likes, comments } = post;
-                console.log(likes)
-                result2.push({
-                    id: id,
-                    content: content,
-                    createAt: createAt,
-                    likeCount: likes.length,
-                    commentCount: comments.length
-                });
-            });
-            return res.status(200).send(result2);
+            // result.map((post)=> {
+            //     const { id, content, createAt, likes, comments } = post;
+            //     console.log(likes)
+            //     result2.push({
+            //         id: id,
+            //         content: content,
+            //         createAt: createAt,
+            //         likeCount: likes.length,
+            //         commentCount: comments.length
+            //     });
+            // });
+            // return res.status(200).send(result2);
         }
     },
     likesCreate: async (req, res) => {
@@ -56,17 +58,37 @@ module.exports = {
             }
         });
         if(!userInfo){
-            res.status(401).json({"message" : "token doesn't exist"});
+            res.status(401).json({ "message" : "token doesn't exist" });
         }
         else {
             await likes.create({
                 userId: userInfo.id,
                 postId: postId
             })
-            res.status(200).send("likesCreate");
+            res.status(200).json({ "message" : "created" });
         }
     },
-    likesDelete: (req, res) => {
-        res.status(200).send("likesDelete");
+    likesDelete: async (req, res) => {
+        const { email, postId } = req.body;
+        let userInfo = await user.findOne({
+            where: {
+                email: email
+            }
+        });
+        if(!userInfo){
+            res.status(401).json({ "message" : "token doesn't exist" });
+        }  
+        else{
+            try{
+                await likes.destroy({
+                where: { userId: userInfo.id,
+                    postId : postId }
+                })
+                return res.status(200).json({ "message" : "delete!" });  
+            } catch(err) {
+                console.log(err);
+                return res.status(500).json({ "message" : "Server Error" });
+            }
+        }
     },
 }

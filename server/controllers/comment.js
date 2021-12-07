cde46829af385edd4da033c488bec7c1fd07f427
wjddbgn <wjddbgn2@gmail.com>
2021-12-07 09:08:29 +0900

    server/comment-get,post

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,20 +1,66 @@
-const {comment, post} = require('../models/index')
+const {comment, post, likes} = require('../models/index')
 
 module.exports = {
     commentUser: async (req, res) => {
-        req.userId = req.userId || 1;
-        let a = await comment.findAll({
-                where:{
-                    userId: req.userId
-                }
+        req.userId = req.userId || 2;
+        try{
+        let a = await post.findAll({
+                include:[
+                    {
+                        model:comment,
+                        attributes:['id'],
+                        where: {
+                            userId:req.userId
+                        }
+                    },
+                    {
+                        model:likes,
+                        attributes:['id']
+                    }
+
+                ]
         })
-        console.log(a)
-        res.send(a)
+        if(a.length === 0) {
+            return res.status(200).json(a)
+        }
+        let b = a.map((el)=> {
+            const {id, content, createAt, likes, comments} = el.dataValues;
+            return ({
+                id:id,
+                content: content,
+                createAt: createAt,
+                likeCount: likes.length,
+                commentCount: comments.length
+            })
+        })
+        return res.send(b)
+        } catch(err) {
+            console.log(err);
+            return res.status(500).json({"message": "Server Error"})
+        }        
     },
-    commentCreate: (req, res) => {
-        res.status(200).send("commentCreate");
+    commentCreate: async (req, res) => {
+        req.userId = req.userId || 1;
+        const {postId, content, commentId} = req.body;
+        try{
+            if(postId && content && commentId) {
+                let a = await comment.create({
+                    userId: req.userId, postId: postId, content:content, commentId: commentId
+                })
+                return res.json({"message": "created!"})
+            }
+                let b = await comment.create({
+                    userId: req.userId, postId: postId, content: content
+                })
+            return res.json(b)
+        } catch(err) {
+            console.log(err);;
+            return res.status(500).json({"message": "Server Error"})
+        }
     },
-    commentDelete: (req, res) => {
+    commentDelete: async (req, res) => {
+        req.userId = req.userId || 1;
+        req.query.commentId = req.query.commentId || 10;
         res.status(200).send("commentDelete");
     },
 }

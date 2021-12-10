e933d5c47f3e84043726460fc61c8e7e923630e2
ksg9482 <ksg930523@gmail.com>
2021-12-10 09:57:10 +0900

    fix:login.tsx

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -66,7 +66,7 @@ function Login(){
                 socialType: userInfo.socialType
             }))
             dispatch(IsLoginHandler(true))
-            dispatch(IsGuestHandler(true))
+            dispatch(IsGuestHandler(false))
             history.push('/Search')
 
         } catch (error:any) {

cce5e08b28dc7c699b653bd08d14a818abeb3d8f
ksg9482 <ksg930523@gmail.com>
2021-12-10 18:09:03 +0900

    fix:update css, fix logout

--- client/src/components/Interest/InterestPost.tsx
+++ client/src/components/Interest/InterestPost.tsx
@@ -1,44 +1,45 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { useSelector, RootStateOrAny } from 'react-redux';
 import { useHistory } from 'react-router';
 import axios from 'axios';
 import Content from '../../pages/Content';
 import styled from 'styled-components';
-//import './style.css'
+import './style.css'
 //날짜별로 나눠야함
 
 export default function InterestPost({ post }: any) {
     //api 넣어야 함
     const history = useHistory()
-    const isMobile = useSelector((state: RootStateOrAny)=>state.IsMobileReducer)
+    const isMobile = useSelector((state: RootStateOrAny) => state.IsMobileReducer.isMobile)
     const clickHandler = async (e: React.MouseEvent<HTMLDivElement>) => {
         const postId = e.currentTarget.id;
         console.log(postId)
         try {
-            //const mypost = await axios.get(`${process.env.REACT_APP_API_URL}/post/${postId}`, { withCredentials: true })
-            //console.log(mypost.data)
+            const mypost = await axios.get(`${process.env.REACT_APP_API_URL}/post/${postId}`, { withCredentials: true })
+            console.log(mypost.data)
             console.log(isMobile)
-            if(isMobile){
-            //모바일인 경우
-                // if(mypost.status === 200){
-                // history.push({
-                //     pathname: '/Content'
-                // })
-            //}
-            console.log('모바일작동')
+            if (isMobile) {
+                //모바일인 경우
+                if(mypost.status === 200){
+                history.push({
+                    pathname: '/Content',
+                    state:{ida: postId}
+                })
+                }
+                console.log('모바일작동')
             }
-            
-            return ;
-        } catch (error:any) {
-            if(error.response.status === 500){
+
+            return;
+        } catch (error: any) {
+            if (error.response.status === 500) {
                 return console.log('서버이상')
             }
             console.log(error.response)
         }
-        
+
         //이거 컨텐트로 보내야 됨
-       
+
     }
 
     const onOffHandler = (key: string) => (e: React.MouseEvent<HTMLDivElement>) => {
@@ -49,7 +50,7 @@ export default function InterestPost({ post }: any) {
         }
     }
 
-    
+
 
 
     const YearMonth = styled.div`
@@ -57,20 +58,24 @@ export default function InterestPost({ post }: any) {
         padding:2px;
     `;
     return (
-        <div>
+        <div className='interest_post_detail_container'>
             {post.map((el: any) => {
                 for (let key in el[0]) {
                     return (//년-월별 컨테이너
                         <div className={`container_${key}`} key={key}>
-                            <YearMonth onClick={onOffHandler(`${key}`)} >{key}</YearMonth>
+                            <div className ='interest_post_detail_container'>
+                            <YearMonth className='interest_post_box' onClick={onOffHandler(`${key}`)} >{key}</YearMonth>
+                            </div>
                             <div>{el[0][key].map((value: any) => {
                                 return (//내용물
-                                    <div className={`container_value_${key} hide`} key={value.id} id={value.id} onClick={clickHandler}>
-                                        <span >{value.createAt}</span>
-                                        <span >{value.content}</span>
+                                    <div>
+                                        <div className={`container_value_${key} hide`} key={value.id} id={value.id} onClick={clickHandler}>
+                                            <div >{value.createAt}</div>
+                                            <div >{value.content}</div>
+                                        </div><br />
                                     </div>
                                 )
-                            })}</div>
+                            })}</div><br />
                         </div>
                     )
                 }

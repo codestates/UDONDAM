c203fa52e46b74202f6e4cd65edf6ee32eddfa60
ksg9482 <ksg930523@gmail.com>
2021-12-14 17:17:33 +0900

    update css

--- client/src/components/Signup/EmailTimer.tsx
+++ client/src/components/Signup/EmailTimer.tsx
@@ -2,9 +2,27 @@ import React,{useState, useEffect} from 'react'
 import { useDispatch, useSelector, RootStateOrAny } from "react-redux";
     
 function EmailTimer() {
+  const [timeCount, setTimeCount] = useState<number>(180);
+  
+  useEffect(() => {
+    const countdown = setInterval(() => {
+        if (timeCount > 0) {
+            setTimeCount(timeCount - 1);
+          }else if (timeCount === 0) {
+              clearInterval(countdown);
+            
+          }
+        }, 1000);
+        return () => clearInterval(countdown); //이게 없으면 인터벌이 여러개 돌아서 숫자가 이상해짐
+  }, [timeCount]);
+
+  const changeForm = function () {
+
+  }
 
   return (
     <>
+    {String(Math.floor(timeCount/60)).padStart(2,'0')}:{Math.floor(timeCount % 60) < 10 ? String(Math.floor(timeCount % 60)).padStart(2,'0') : String(Math.floor(timeCount % 60)).padEnd(2,'0')}
     </>
   )
 }
--- client/src/pages/Footer.tsx
+++ client/src/pages/Footer.tsx
@@ -1,16 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-    //네비게이션바 로그인/게스트 분리할것, 홈,로그인,회원가입창에선 안나옴
-function Footer () {
+import './styles/FooterStyle.css'
+//네비게이션바 로그인/게스트 분리할것, 홈,로그인,회원가입창에선 안나옴
+function Footer() {
 
-    return(
-            <div className='grid_footer_container'>
-                <div></div>
-                <div></div>
-                <div></div>
-               footer내용 들어갈 자리
+    const teamMembers = [
+        {id:1, name:'김석규', git:'ksg9482'},
+        {id:2, name:'정동교', git:'wjdehdry3'},
+        {id:3, name:'이선영', git:'siri-syl'},
+        {id:4, name:'정동현', git:'wjddbgn'},
+    ]
+
+    return (
+        <footer className='grid_footer_container'>
+            <div className='grid_footer_about_place'>
+                <div className='grid_footer_about_box'>
+                <div className='grid_footer_about'>
+                    UDONDAM - 우리 동네 이야기
+                </div>
+                </div>
             </div>
-            
+            <div className='grid_footer_copyright_place'>
+            <div className='grid_footer_copyright_box'>
+                <div className='grid_footer_copyright'>
+                    copyright
+                </div>
+                </div>
+            </div>
+            <div className='grid_footer_member_place'>
+                <div className='grid_footer_member_box'>
+                    {teamMembers.map((memeber)=>{
+                        return(
+                            <div className='grid_footer_member' key={memeber.id}>
+                                <div className='grid_footer_member_name footer_text'>{memeber.name}</div>&nbsp;&nbsp;
+                                <div className='grid_footer_member_git footer_text'>https://github.com/{memeber.git}</div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </footer>
+
     )
 }
 

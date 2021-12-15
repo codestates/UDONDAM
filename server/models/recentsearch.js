d5b8890bd989debe705584cbaf11a61284f94e5a
siri-syl <sylee010101@gmail.com>
2021-12-15 18:32:02 +0900

    FIX: table recentsearch - 오탈자 수정했습니다.

--- server/models/recentsearch.js
+++ server/models/recentsearch.js
@@ -3,28 +3,31 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class recentSearch extends Model {
+  class recentsearch extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      models.recentSerch.belongsTo(models.user, {
+      models.recentsearch.belongsTo(models.user, {
         foreignKey: 'userId',
         sourceKey: 'id',
         onDelete: 'CASCADE',
       });
     }
   };
-  recentSearch.init({
+  recentsearch.init({
     userId: DataTypes.INTEGER,
     tag: DataTypes.STRING,
     nottag: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'recentSearch',
+    modelName: 'recentsearch',
+    freezeTableName: true,
+    timestamps: true,
+    createdAt: "createAt",
+    updatedAt: false
   });
-  return recentSearch;
+  return recentsearch;
 };
